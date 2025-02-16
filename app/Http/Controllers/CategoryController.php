8b082af6f553ac2017434850d06136f4ceb5f963
unknown <heyreza8@gmail.com>
2025-02-17 01:12:40 +0330

    Feat Category CRUD

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Http\Resources\Category\CategoryApiResource;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::oldest()->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => CategoryApiResource::collection($categories),
+            'message' => 'عملیات با موفقیت انجام شد'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $inputs = $request->validated();
+
+        $category = Category::create($inputs);
+
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryApiResource($category),
+            'message' => 'عملیات با موفقیت انجام شد'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryApiResource($category),
+            'message' => 'عملیات با موفقیت انجام شد'
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $inputs = $request->validated();
+
+        $result = $category->update($inputs);
+
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryApiResource($category->refresh()),
+            'message' => 'عملیات با موفقیت انجام شد'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $result = $category->delete();
+
+        return response()->json([
+            'success' => true,
+            'data' => [],
+            'message' => 'عملیات با موفقیت انجام شد'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('category')->controller(CategoryController::class)->group(function(){
+    Route::get('/','index');
+    Route::post('create','store');
+    Route::get('/{category}','show');
+    Route::put('/update/{category}','update');
+    Route::delete('/delete/{category}','destroy');
+});
